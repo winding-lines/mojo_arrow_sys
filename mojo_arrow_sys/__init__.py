84918010fa77ca6305f78961713304349ade5ba4
Marius S <39998+winding-lines@users.noreply.github.com>
2025-04-05 02:05:30 +0300

    Migrate to get the version with scmtools

--- mojo_arrow_sys/__init__.py
+++ mojo_arrow_sys/__init__.py
@@ -1,3 +1,5 @@
-from .mojo_arrow_sys import arrow_stream
-
-__version__ = "0.1.1"
+try:
+    from ._version import version as __version__
+except ImportError:
+    # Package is not installed
+    __version__ = "0.0.0+unknown"
--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ mojo_arrow_sys_module = Extension(
     extra_compile_args=["-Wall", "-Wextra"],
 )
 
+
 setup(
     name="mojo_arrow_sys",
-    version="0.1.1",
     description="Python C extension for accessing Arrow C streams from Mojo",
     author="Marius Seritan",
     author_email="pypi@seritan.net",
     url="https://github.com/winding-lines/mojo_arrow_sys",
     ext_modules=[mojo_arrow_sys_module],
-    python_requires=">=3.7",
+    python_requires=">=3.10",
 )
