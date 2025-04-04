2f0a0e869f050cadb947d1fbf9018b2aef50ed50
Marius S <39998+winding-lines@users.noreply.github.com>
2025-04-04 12:14:09 +0200

    Fix name, email and github links.

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ setup(
     name='mojo_arrow_sys',
     version='0.1.0',
     description='Python C extension for accessing Arrow C streams from Mojo',
-    author='Mihai Seritan',
-    author_email='mihai.seritan@example.com',
-    url='https://github.com/mseritan/mojo_arrow_sys',
+    author='Marius Seritan',
+    author_email='pypi@seritan.net',
+    url='https://github.com/winding-lines/mojo_arrow_sys',
     ext_modules=[mojo_arrow_sys_module],
     python_requires='>=3.7',
 )
